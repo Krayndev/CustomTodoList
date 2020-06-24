a7ba7e01aac201c1656b5ea11a74508255256876
Krayndev <kenrayner3@gmail>
2020-06-24 14:00:36 -0500

    TodoList Version 1.00; fully functional, complete application

---
+++ TodoList/src/Launcher.java
@@ -0,0 +1,16 @@
+
+/*
+    Krayndev; 06/23/2020
+ */
+
+/*
+    This class is meant to instantiate the main list class only.
+ */
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        Main main = new Main("Todolist App", 600, 480);
+    }
+}
